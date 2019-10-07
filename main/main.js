91897a94565e58e1ba8b6bb3928878601cf8f427
Dan Luigi Tating <danluigitating@hotmail.com>
2019-10-08 04:23:30 +0800

    Pushed changes for main.js

--- main/main.js
+++ main/main.js
@@ -1,74 +1,54 @@
 module.exports = function main(input) {
     console.log("Debug Info");
-    
-    inputs = [
-        {
-            Barcode: 'ITEM000000',
-            Name: 'Coca-Cola',
-            Unit: 'bottle',
-            Price: 3.00
-    
-        },
-        {
-            Barcode: 'ITEM000000',
-            Name: 'Coca-Cola',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000000',
-            Name: 'Coca-Cola',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000000',
-            Name: 'Coca-Cola',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000000',
-            Name: 'Coca-Cola',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000001',
-            Name: 'Sprite',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000001',
-            Name: 'Sprite',
-            Unit: 'bottle',
-            Price: 3.00
-        },
-        {
-            Barcode: 'ITEM000004',
-            Name: 'Battery',
-            Unit: 'a',
-            Price: 2.00
-        }
-    ];
-
-    const totalPrice= inputs.map(inputs => inputs.Price)
-                             .reduce((a, b) => a + b, 0);
-    
-    
-    const Barcode= inputs.map(inputs => inputs.Barcode);
-
-    let finalReceipt =
-    '***<store earning no money>Receipt ***\n' +
-    'Name: Coca-Cola, Quantity: 5 bottles, Unit price: 3.00 (yuan), Subtotal: 15.00 (yuan)\n' +
-    'Name: Sprite, Quantity: 2 bottles, Unit price: 3.00 (yuan), Subtotal: 6.00 (yuan)\n' +
-    'Name: Battery, Quantity: 1, Unit price: 2.00 (yuan), Subtotal: 2.00 (yuan)\n' +
-    '----------------------\n' +
-    'Total: 23.00 (yuan)\n' +
-    '**********************\n';
+    let receipt ='';
+    receipt = printInventory(input);
+    return receipt;
+}
 
 
-    console.log(pricesArray);
-    return input;
+function printInventory(input){
+    const totalPrice= input.map(input => input.Price)
+                           .reduce((a, b) => a + b, 0);
+
+    let itemCount = new Object();
+
+    for(let i = 0; i < input.length; i++) {
+        if(itemCount[input[i].Name] != null) {
+        itemCount[input[i].Name] += 1;
+       } else {
+        itemCount[input[i].Name] = 1;
+       }
+    }
+
+    let itemPrize = new Object();
+
+    for(let i = 0; i < input.length; i++) {
+        if(itemPrize[input[i].Name] == null) {
+        itemPrize[input[i].Name]= input[i].Price;
+       } 
+    }
+
+    let itemString= '';
+    let header= '***<store earning no money>Receipt ***\n';
+    let footer1= '----------------------\n';
+    let footer2= 'Total: ' + totalPrice.toFixed(2) + ' (yuan)\n';
+    let footer3= '**********************\n';
+    for(let i = 0; i < Object.keys(itemCount).length; i++) {
+        let quantity= Object.values(itemCount)[i];
+        if(Object.values(itemCount)[i]>1)
+            quantity+= ' bottles';
+
+        let prize;
+        if(Object.keys(itemCount)[i]===Object.keys(itemPrize)[i])
+            prize= ', Unit price: ' + Object.values(itemPrize)[i].toFixed(2) + ' (yuan),';
+
+        let subtotal= ' Subtotal: ' + (Object.values(itemCount)[i] * Object.values(itemPrize)[i]).toFixed(2) 
+            + ' (yuan)';
+        
+        itemString += 'Name: ' + Object.keys(itemCount)[i] + ', Quantity: ' + quantity 
+        + prize + subtotal + '\n';
+    }
+    
+    let receipt= header+ itemString + footer1 + footer2 + footer3 ;
+    return receipt;
 };
